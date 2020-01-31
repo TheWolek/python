84df78f4aeab78762d2ba4c5647f23b1a69faf4d
TheWolek <pawelm916@wp.pl>
2020-01-31 22:01:01 +0100

    teoriaWpraktyce

---
+++ basic/basic.py
@@ -0,0 +1,20 @@
+# Python jest wysoko poziomowym, interpretowanym, uniwersalnego przeznaczenia językiem
+# Typy podstawowych zmiennych: 
+# int, float, string, bool
+
+# Typy złożonych zmiennych: 
+# list = [12,'abc',true,['xyz']]
+# list[1] => 'abc'  list[3] => ['xyz']  list[3][0] => 'xyz'
+# list[0:2] => [12,'abc',true]
+
+# dictionary = {
+#   'name': 'john',
+#   'age': 20
+# }
+# dictionary['name'] => 'john'
+# dictionary['surname'] = 'kowalski'
+# del dictionary['surname']
+
+# tuple = (12,)
+# tuple = ('abc',20)
+# tuple.append('xyz') => ERROR
