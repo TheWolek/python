3c2ab8ae2601c589d0f41a604fad2bccb4cb3f16
TheWolek <pawelm916@wp.pl>
2020-02-01 01:09:30 +0100

    file teoria

---
+++ file/theory.py
@@ -0,0 +1,18 @@
+# 1. Open
+# 2. Read or Write
+# 3. Close file
+
+# f = open('test.txt','rw')
+# f = open('test2.txt','rwx') jesli plik nie istnieje utworzy go
+
+# f.close()
+
+#  with open('test.txt','rw') as f:
+#      f.write('first line\n')
+#      f.write('second line')
+
+# f = open('test.txt','r')
+# f.read() => cały plik
+
+# for line in f:
+#   print(line, end = '')
