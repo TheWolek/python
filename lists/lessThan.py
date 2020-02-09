29b09a2813bced7730f68316d4ebc38e435cde27
TheWolek <pawelm916@wp.pl>
2020-02-09 21:32:18 +0100

    lessThan; rockPaperScisors

---
+++ lists/lessThan.py
@@ -0,0 +1,7 @@
+# Napisz funckje wypisującą zawartość listy z elementami listy nums mniejszymi od 5
+nums = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+def lessTan():
+    pass
+
+lessTan()
---
+++ mini-projects/rockPaperScisors.py
@@ -0,0 +1,8 @@
+# Napisz grę w papier, kamień, nożyce
+# Poproś graczy o wpisanie papier / kamień / nożyce
+# następnie je porównaj
+# pamiętaj o zasadach:
+# kamień bije nożyce 
+# papier bije kamień 
+# nożyce biją papier
+
