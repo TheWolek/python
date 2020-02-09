4fe6b69f2127270b5a2e349d9b498297004e46c9
TheWolek <pawelm916@wp.pl>
2020-02-09 11:16:10 +0100

    pliki pliki; binarne najwieksza najmniejsza

--- najmniejInajwiek.py
+++ najmniejInajwiek.py
@@ -7,5 +7,14 @@ f.close()
 # print(lines[3],lines[4])
 # print(len(lines[3])-1,len(lines[4])-1)
 # print(lines.index(lines[3]))
+count = 0
+for num in lines:
+    lines[count] = lines[count].strip()
+    lines[count] = int(lines[count],2)
+    count+=1
+big = max(lines)
+mini = min(lines)
 
-biggest = int(lines[0],2)
+# print(big)
+print(lines.index(big)+1)
+print(lines.index(mini)+1)
