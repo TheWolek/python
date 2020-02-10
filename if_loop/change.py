499264b29259c811335c2e096339bde52a549cc5
TheWolek <pawelm916@wp.pl>
2020-02-10 21:40:01 +0100

    change > fnc

--- if_loop/change.py
+++ if_loop/change.py
@@ -24,46 +24,63 @@ def policzKoszyk():
     for product in basket:
         print(product[0],product[1],sep=' : ')
         total += product[1]
-    print('W sumie:',total) 
+    return total
 
-#=============================
-
-print('Wybierz produkty wpisując ich ID')
-products = {
-    1: ('piwo',3.5),
-    2: ('pizza',35.0),
-    3: ('ser żółty',3.5),
-    4: ('tarta',10.0),
-    5: ('bułka słodka',2.9)
-}
-
-for key, product in products.items():
-    print(key,'.',product[0],' : ',product[1],sep='')
-
-print('wpisz exit jeśli nie chcesz już kupować')
-
-wyborProduktow()
-policzKoszyk()
-
-total2 = int(total)
-while total2%10 != 0:
-    total2+=1
-print(1,total2,sep='. ')
-print(2,total2+10,sep='. ')
-Changes=(total2,total2+10)
-selecting = True
-while selecting:
-    try:
-        select = int(input('z ilu wydać? (1,2): '))
-        if(select in [1,2]):
-            selecting = False
-        else:
+def ZiluReszta():
+    global total2,Changes
+    total2 = int(total)
+    while total2%10 != 0:
+        total2+=1
+    print(1,total2,sep='. ')
+    print(2,total2+10,sep='. ')
+    Changes=(total2,total2+10)
+    selecting = True
+    while selecting:
+        try:
+            select = int(input('z ilu wydać? (1,2): '))
+            if(select in [1,2]):
+                selecting = False
+                return Changes[select-1]
+            else:
+                print('podaj poprawną liczbę (1-2): ')
+                selecting = True
+        except ValueError:
             print('podaj poprawną liczbę (1-2): ')
             selecting = True
-    except ValueError:
-        print('podaj poprawną liczbę (1-2): ')
-        selecting = True
 
-policzKoszyk()
-change = round(Changes[select-1]-total,2)
-print('Suma:',total,'Do wydania z:',Changes[select-1],'Reszta:',change)
+def init():
+    global products,basket,total,total2,Changes
+    # mozna też pobrać dane z zewnętrznego pliku i stworzyć poniższą listę c:
+    products = {
+        1: ('piwo',3.5),
+        2: ('pizza',35.0),
+        3: ('ser żółty',3.5),
+        4: ('tarta',10.0),
+        5: ('bułka słodka',2.9)
+    }
+    basket = []
+    total = 0
+    total2 = 0
+    Changes = ()
+
+
+# =================================================
+def main():
+    print('Wybierz produkty wpisując ich ID')
+    init()
+
+    # wyświetlenie listy
+    for key, product in products.items():
+        print(key,'.',product[0],' : ',product[1],sep='')
+
+    print('wpisz exit jeśli nie chcesz już kupować')
+
+    wyborProduktow()
+    total = policzKoszyk()
+    print('W sumie:',total) 
+    ChangeSelection = ZiluReszta()
+    total = policzKoszyk()
+    change = round(ChangeSelection-total,2)
+    print('Suma:',total,'Do wydania z:',ChangeSelection,'Reszta:',change)
+
+main()
