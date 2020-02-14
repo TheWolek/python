cf1875328813c3fe8b725c16aa9d27b04912f2ae
TheWolek <pawelm916@wp.pl>
2020-02-14 14:34:54 +0100

    factorial

--- if_loop/factorial.py
+++ if_loop/factorial.py
@@ -1,4 +1,13 @@
 # Napisz funkcje, która oblicza silnie z podanej liczby
 # liczba wpisywana jest z klawiatury
 
-# 3! = 3 * 2 * 1
+# 3! = 1 * 2 * 3 = 6
+
+def factorial(n):
+    fact = 1
+    for x in  range(2,n+1): # 2..3..stop9*
+        fact *= x
+    return fact
+
+print(factorial(3))
+print(factorial(10))
